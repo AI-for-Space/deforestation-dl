3e6951304723c7523294de247c0ff2e4e5ebfa49
nestorcamposg <campos@ice.csic.es>
2023-10-08 14:42:33 +0200

    WIP #1. Add store images in local

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 from src.data_loading import DataLoader
+from src.segmentation import Segmentator
 
 
 def main():
-    print("Hola mundo")
 
     INSTACE_ID = '98e90c95-04e9-4aa8-a105-688da74595be'
     SH_CLIENT_ID = '1c7168a0-37b5-444f-a7e6-826ae6c19d90'
     SH_CLIENT_SECRET = 'f&}UR;bV(I)fx?r|:hlNZ0sK1utD4ny_4V0WsQzJ'
 
     loader = DataLoader(INSTACE_ID,SH_CLIENT_ID,SH_CLIENT_SECRET)
-    loader.get_image(-4.447581,-54.980870)
+    segmentator = Segmentator()
+    loader.get_image(-3.764226,-52.128754, 400)
     loader.display_random_samples(3)
-
+    #loader.segment_fast_sam(2018,4)
+    print("Jello")
 
 
 
--- src/data_loading.py
+++ src/data_loading.py
@@ -4,6 +4,11 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 import random
+import cv2
+from skimage import morphology
+from scipy.ndimage import binary_fill_holes
+
+from models.FastSAM.fastsam import FastSAM, FastSAMPrompt
 
 from sentinelhub import (
     SHConfig,
@@ -34,7 +39,7 @@ class DataLoader:
             function setup() {
                 return {
                     input: [{
-                        bands: ["B02", "B03", "B04"]
+                        bands: ["B02", "B03", "B04", "B08"]
                     }],
                     output: {
                         bands: 3
@@ -49,30 +54,26 @@ class DataLoader:
         self.year_data={}
 
     def get_true_color_request(self,time_interval):
-
-        print(f"Image shape at {self.resolution} m resolution: {self.betsiboka_size} pixels")
-
         return SentinelHubRequest(
             evalscript=self.evalscript_true_color,
             input_data=[
                 SentinelHubRequest.input_data(
-                    data_collection=DataCollection.SENTINEL2_L1C,
+                    data_collection=DataCollection.SENTINEL2_L2A,
                     time_interval=time_interval,
                     mosaicking_order=MosaickingOrder.LEAST_CC,
                     maxcc = 0.3, #cloud coverage
                 )
             ],
             responses=[SentinelHubRequest.output_response("default", MimeType.PNG)],
-            bbox=self.betsiboka_bbox,
-            size=self.betsiboka_size,
+            bbox=self.image_bbox,
+            size=self.image_size,
             config=self.sentinel_hub_config,
         )
 
-    def get_image(self, latitude, longitude):
-
+    def get_image(self, latitude, longitude, size_fragments):
         # Define the desired width and height of the bounding box in degrees
-        bbox_width = 0.6  # Example: 0.1 degrees (approximately 11.1 km at the equator)
-        bbox_height = 0.6
+        bbox_width = 0.3  # Example: 0.1 degrees (approximately 11.1 km at the equator)
+        bbox_height = 0.3
 
         # Calculate the bounding box coordinates
         lower_left_lat = latitude - bbox_height / 2
@@ -81,39 +82,59 @@ class DataLoader:
         upper_right_lon = longitude + bbox_width / 2
 
         # Create the bounding box
-        self.betsiboka_coords_wgs84 = (lower_left_lon, lower_left_lat, upper_right_lon, upper_right_lat)
-        self.resolution = 30
-        self.betsiboka_bbox = BBox(bbox=self.betsiboka_coords_wgs84, crs=CRS.WGS84)
-        self.betsiboka_size = bbox_to_dimensions(self.betsiboka_bbox, resolution=self.resolution)
+        self.image_coords_wgs84 = (lower_left_lon, lower_left_lat, upper_right_lon, upper_right_lat)
+        self.resolution = 20
+        self.image_bbox = BBox(bbox=self.image_coords_wgs84, crs=CRS.WGS84)
+        self.image_size = bbox_to_dimensions(self.image_bbox, resolution=self.resolution) # Size of the final image
         
+        # Create Time Intervals
         start_year = 2017
         end_year = 2023
         month = [7,8]
-        time_intervals = []
+
+        data = []
     
         for year in range(start_year, end_year + 1):
-            start_date = datetime.datetime(year, month[0], 1)
-            end_date = datetime.datetime(year, month[1], 31)
-                
-            time_intervals.append((start_date.date().isoformat(), end_date.date().isoformat()))
 
-        print(time_intervals)
+            # Generate filename for image
+            filedir = f"dataset/{year}"
+            filename = f"Latitude_{round(latitude,2)}_Longitude_{round(longitude,2)}.png"
+            filepath = os.path.join(os.getcwd(),filedir, filename)
+
+            # Create the output directory if it doesn't exist
+            os.makedirs(filedir, exist_ok=True)
+
+            # Check if image exists
+            if os.path.exists(filepath):
+                print(f"Image for year {year} already stored")
+                data.append(cv2.imread(filepath))
+            
+            else:
+                start_date = datetime.datetime(year, month[0], 1)
+                end_date = datetime.datetime(year, month[1], 31)
+                time_interval = (start_date.date().isoformat(), end_date.date().isoformat())
+                image =  self.get_true_color_request(time_interval).get_data()[0]
+                print(f"Getting image from Sentinel Hub from year {year}")
+                data.append(image)
+                cv2.imwrite(filepath, image)
+                
 
         # create a list of requests
-        list_of_requests = [self.get_true_color_request(slot) for slot in time_intervals]
-        list_of_requests = [request.download_list[0] for request in list_of_requests]
+        #list_of_requests = [self.get_true_color_request(slot) for slot in time_intervals]
+        #list_of_requests = [request.download_list[0] for request in list_of_requests]
 
         # download data with multiple threads
-        data = SentinelHubDownloadClient(config=self.sentinel_hub_config).download(list_of_requests, max_threads=5)
+        #data = SentinelHubDownloadClient(config=self.sentinel_hub_config).download(list_of_requests, max_threads=5)
 
         # Loop through each time interval
         year = start_year
         for image in data:
             fragment_list = []
-            for y in range(0, self.betsiboka_size[1], 200):
-                for x in range(0, self.betsiboka_size[0], 200):
-                    fragment = image[y:y+200, x:x+200, :]
-                    fragment_list.append(fragment)
+            for y in range(0, self.image_size[1], size_fragments):
+                for x in range(0, self.image_size[0], size_fragments):
+                    fragment = image[y:y+size_fragments, x:x+size_fragments, :]
+                    if fragment.shape[0] == size_fragments and fragment.shape[1] == size_fragments:
+                        fragment_list.append(fragment)
             # Store fragments in arrays by year
             self.year_data[year] = fragment_list
             year = year + 1
@@ -125,7 +146,8 @@ class DataLoader:
         fig, axs = plt.subplots(ncols=ncols, nrows=nrows, figsize=(5 * ncols , 5 * nrows))
 
         for i in range(nrows):
-            index_random_image = random.randint(0, 50)
+            number_of_fragments = len(next(iter(self.year_data.values()))) # Number of sample of array of the first year
+            index_random_image = random.randint(0, number_of_fragments)
             for j,year in enumerate(self.year_data.keys()):
                 # Get a random image from the selected year
                 ax = axs[i, j]
@@ -136,4 +158,3 @@ class DataLoader:
         
         plt.tight_layout()
         plt.show()
-
